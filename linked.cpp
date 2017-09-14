d94d11a553d67b636cb22497a8dfc89af1a236a5
shafik <shafik.te@gmail.com>
2017-09-14 16:12:30 -0700

    added doubly linked list

---
+++ linked.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+struct node{
+    int data;
+    node* next;
+    node* prev;
+};
+node* head = NULL;
+node* GetNode(int x){
+    node* newNode = new node();
+    newNode->data = x;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
+}
+
+void insertHead(int value){
+    node* newNode = GetNode(value);
+    if(head==NULL){
+        head = newNode;
+        return;
+    }
+    head->prev = newNode;
+    newNode->next = head;
+    head = newNode;
+}
+void print(){
+    node*temp = head;
+    while(temp!=NULL){
+        cout<< temp->data<< " ";
+        temp = temp->prev;
+    }
+}
+
+int main(){
+    insertHead(2);
+    insertHead(3);
+    print();
+return 0;
+}
